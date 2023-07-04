489b2473a9337f859f501fa6c05f42465afa8d1c
Sunny98351 <125554605+Sunny98351@users.noreply.github.com>
2023-07-04 20:45:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+provider "azurerm" {
+    subscription_id = "63624c86-fe10-49d1-b0c7-b5b0be84da6a"
+    client_id = "27ed852a-f10d-41bc-951c-d12bca932c28"
+    client_secret = "Wvi8Q~13MgHl609Cm6Ba6F-774N3uUnk5dECGb0F"
+    tenant_id = "7270ce39-4b64-4579-8f7f-93639d71f1ca"
+
+    features {
+      
     }
-  }
+  
 }
 resource "azurerm_resource_group" "adi-t2" {
     name = var.resource_group_name
